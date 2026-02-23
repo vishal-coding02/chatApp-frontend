c10f9c071fad0257a011226514569ca0e7687b6e
unknown <vshrivastav933@gmail.com>
2026-02-23 22:06:46 +0530

    added protectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: any) => {
-  const user = useSelector((state: any) => state.auth.user);
+  const user = useSelector((state: any) => state.auth.userData);
 
   if (!user) {
     return <Navigate to="/" />;
