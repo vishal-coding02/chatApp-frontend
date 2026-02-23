bc78a94ba0a863ec556d15283aca96bb1e0fde3c
unknown <vshrivastav933@gmail.com>
2026-02-23 22:06:14 +0530

    added protectedRoute

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: any) => {
+  const user = useSelector((state: any) => state.auth.user);
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,20 @@ import Home from "./pages/v1/Home";
 import Signup from "./pages/v1/Signup";
 import Login from "./pages/v1/Login";
 import ChatLayout from "./components/layouts/v1/ChatLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/auth/signup", element: <Signup /> },
   { path: "/auth/login", element: <Login /> },
-  { path: "/chatscreen", element: <ChatLayout /> },
+  {
+    path: "/chatscreen",
+    element: (
+      <ProtectedRoute>
+        <ChatLayout />
+      </ProtectedRoute>
+    ),
+  },
 ]);
 
 export default router;
